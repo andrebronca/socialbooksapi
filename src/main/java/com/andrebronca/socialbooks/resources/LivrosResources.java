f5a0483a7f80f29cf8fafb96c8f05827c941ad9d
Andre Bronca <bronca.andre@gmail.com>
2018-05-05 15:56:54 -0300

    Manipulando create and find

--- src/main/java/com/andrebronca/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/andrebronca/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,18 @@
 package com.andrebronca.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andrebronca.socialbooks.domain.Livro;
 import com.andrebronca.socialbooks.repository.LivrosRepository;
@@ -26,18 +30,29 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {	
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {	
+		livro = livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public Optional<Livro> buscar(@PathVariable("id") Long id) {
-		return livrosRepository.findById(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro = livrosRepository.findOne(id);
+		
+		if (livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public void deletar(@PathVariable("id") Long id) {
-		livrosRepository.deleteById(id);
+		livrosRepository.delete(id);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
