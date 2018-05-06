9b10fcaec5a6a1dd5182b48e481f51f8dd5c7c2c
Andre Bronca <bronca.andre@gmail.com>
2018-05-06 17:53:20 -0300

    Autenticacao básica

--- src/main/java/com/andrebronca/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/andrebronca/socialbooks/resources/LivrosResources.java
@@ -12,6 +12,8 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,6 +73,11 @@ public class LivrosResources {
 	
 	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
 	public ResponseEntity<Void> adicionarComentario(@Valid @PathVariable("id") Long livroId, @RequestBody Comentario comentario) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		comentario.setUsuario(auth.getName());
+		
 		livrosService.salvarComentario(livroId, comentario);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
